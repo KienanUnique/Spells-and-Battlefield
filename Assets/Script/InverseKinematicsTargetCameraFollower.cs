d6a2e3553f460161b7a2ac4c7e67633ebc9b73a8
KienanUnique <69349336+KienanUnique@users.noreply.github.com>
2023-04-21 00:40:44 +0500

    Add IK camera following to player body

---
+++ Assets/Script/InverseKinematicsTargetCameraFollower.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class InverseKinematicsTargetCameraFollower : MonoBehaviour
+{
+    [SerializeField] private Transform _cameraTransform;
+    [SerializeField] private Transform _bodyTransform;
+    [SerializeField] private float _needDistance;
+    private Transform _cashedTransform;
+    private Vector3 _needDirection;
+
+    private void Awake()
+    {
+        _cashedTransform = transform;
+    }
+
+    private void Update()
+    {
+        _needDirection = Vector3.Reflect(_cameraTransform.forward, _bodyTransform.up);
+        _cashedTransform.position = _needDistance * _needDirection + _bodyTransform.position;
+    }
+}
