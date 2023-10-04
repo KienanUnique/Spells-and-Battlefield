7fd2bed938f70eb80ffc29b16dceddd826fcef82
KienanUnique <69349336+KienanUnique@users.noreply.github.com>
2023-10-05 00:37:56 +0500

    Add post processing for menus, add view component for buttons on 3d objects and add 3d ui items prefabs

---
+++ Assets/Script/UI/UI3DButtonView.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace UI
+{
+    public class UI3DButtonView : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+    {
+        private const string EmissionKeyword = "_EMISSION";
+        [SerializeField] private Renderer _renderer;
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            _renderer.material.EnableKeyword(EmissionKeyword);
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            _renderer.material.DisableKeyword(EmissionKeyword);
+        }
+    }
+}
